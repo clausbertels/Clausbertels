03ca2566d97f26f230a9873e40e4d0e8b0916b35
clausbertels <claus.bertels@gmail.com>
2017-02-01 16:15:22 +0100

    comments toegevoegd denk ik

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8" />
+    <title>Claus Bertels</title>
+    <link rel="stylesheet" href="" />
+<!--    Adobe Typekit-->
+    <script src="https://use.typekit.net/gye7ujw.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+</head>
+    
+<body>
+    <h1>Welcome!</h1>
+    <p>Lorem schmipsum</p>
+    <ul>
+        <li><a href="CV">CV</a></li>
+        <li><a href="portfolio">Portfolio</a></li>
+        <li><a href="thoughts">Thoughts</a></li>
+    </ul>
+</body>
+</html>
